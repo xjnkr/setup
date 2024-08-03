15240e9e98ea7dd65a657c7b5fa50b9bbbd6792c
r1614 <ryuyijun@naver.com>
2024-08-03 17:02:25 +0900

    Update homarr.sh

--- homarr.sh
+++ homarr.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p /data/homarr
+
+echo "made /data/homarr"
+
+cd /data/homarr
+
 wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homarr.yml
 
 echo "downloaded docker-compose.yml"
