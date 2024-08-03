9d8a2403d437b2fde330bcecd9c51581ed1c96ca
r1614 <ryuyijun@naver.com>
2024-08-03 16:33:21 +0900

    Create filebrowser.sh

---
+++ filebrowser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir -p /data/filebrowser/config
+
+echo "made necessary directory"
+
+touch /data/filebrowser/config/database.db
+
+echo "made empty database.db"
+
+cd /data/filebrowser/config
+
+wget https://raw.githubusercontent.com/r1614/setup/main/filebrowser.json
+
+echo "downloaded filebrowser.json"
+
+cd /data/filebrowser
+
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/filebrowser.yml
+
+echo "downloaded docker-compose.yml"
+
+docker compose up -d
+
+echo "finished installing filebrowser"
+
+echo "exiting..."
+
+exit
