86c94b54afad559921c7eb2edf66bd77ed804639
Ryuyijun <ryuyijun@naver.com>
2024-08-04 11:08:31 +0900

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf dashdot.sh
+rm -rf dockge.sh
+rm -rf dozzle.sh
+rm -rf excalidraw.sh
+rm -rf filebrowser.sh
+rm -rf full.sh
+rm -rf homarr.sh
+rm -rf npmplus.sh
+rm -rf setup.sh
+rm -rf delete.sh
+
+echo "finished delete"
+
+echo "exiting..."
+
+exit
