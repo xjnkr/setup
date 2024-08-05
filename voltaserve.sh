f5af65a714c97578c47b8e7df120980b10368be8
Ryuyijun <ryuyijun@naver.com>
2024-08-05 23:17:34 +0900

    Update voltaserve.sh
    add ascii, git > /dev/null

--- voltaserve.sh
+++ voltaserve.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo __      __   _ _                                
+echo \ \    / /  | | |                               
+echo  \ \  / /__ | | |_ __ _ ___  ___ _ ____   _____ 
+echo   \ \/ / _ \| | __/ _` / __|/ _ \ '__\ \ / / _ \
+echo    \  / (_) | | || (_| \__ \  __/ |   \ V /  __/
+echo     \/ \___/|_|\__\__,_|___/\___|_|    \_/ \___|
+                                                                                                
 cd /data
 
-git clone https://github.com/kouprlabs/voltaserve.git
+git clone https://github.com/kouprlabs/voltaserve.git > /dev/null
 
 echo "clone voltaserve git"
 
@@ -11,7 +18,5 @@ cd voltaserve
 docker compose up -d
 
 echo "finished install voltasrver"
-
-echo "exiting..."
 
 exit
