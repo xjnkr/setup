d2b29aa14af0679dcd7996e5873842c4f8e257a7
Ryuyijun <ryuyijun@naver.com>
2024-08-05 23:15:26 +0900

    Update npmplus.sh

--- npmplus.sh
+++ npmplus.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+echo  _   _ _____  __  __        _____  _           
+echo | \ | |  __ \|  \/  |      |  __ \| |          
+echo |  \| | |__) | \  / |______| |__) | |_   _ ___ 
+echo | . ` |  ___/| |\/| |______|  ___/| | | | / __|
+echo | |\  | |    | |  | |      | |    | | |_| \__ \
+echo |_| \_|_|    |_|  |_|      |_|    |_|\__,_|___/
+                                                                                                
 mkdir -p /data/npmplus
 
 echo "made /data/npmplus"
 
 cd /data/npmplus
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/npmplus.yml
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/npmplus.yml > /dev/null
 
 echo "downloaded docker-compose.yml"
 
 docker compose up -d
 
 echo "finishing install nginxproxymanager-plus"
-
-echo "don't forget to portforwarding 5480,8543"
-
-echo "admin page is ip:81"
-
-echo "exiting..."
 
 exit
 
