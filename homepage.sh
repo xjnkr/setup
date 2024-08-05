eb7eb02352289ed50b6c8a3d1b509b1fe8c8de89
Ryuyijun <ryuyijun@naver.com>
2024-08-05 21:19:49 +0900

    Create homepage.sh

---
+++ homepage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p /data/homepage/config
+
+echo 'made /data/homepage/config'
+
+cd /data/homepage/config
+
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homepage.yml
+
+echo 'downloaded docker-compose.yml'
+
+docker compose up -d
+
+echo 'finished'
+
+echo "homepage is ip:100"
+
+echo 'exiting...'
+
+exit
+
