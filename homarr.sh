f11ccb172573aeb4d95be3ebb4a410bf592eee08
Ryuyijun <ryuyijun@naver.com>
2024-08-05 23:12:10 +0900

    Update homarr.sh

--- homarr.sh
+++ homarr.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+echo  _    _                                 
+echo | |  | |                                
+echo | |__| | ___  _ __ ___   __ _ _ __ _ __ 
+echo |  __  |/ _ \| '_ ` _ \ / _` | '__| '__|
+echo | |  | | (_) | | | | | | (_| | |  | |   
+echo |_|  |_|\___/|_| |_| |_|\__,_|_|  |_|   
+                                         
+                                         
+
 mkdir -p /data/homarr
 
 echo "made /data/homarr"
 
 cd /data/homarr
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homarr.yml
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homarr.yml > /dev/null
 
 echo "downloaded docker-compose.yml"
 
 docker compose up -d
 
 echo "finished install homarr"
-
-echo "homarr is ip:7575"
-
-echo "exiting..."
 
 exit
