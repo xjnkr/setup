c7b8567955963475eb01343bd2dc6afc65c0506f
Ryuyijun <ryuyijun@naver.com>
2024-08-05 23:06:25 +0900

    Update dozzle.sh
    add ascii , wget > /dev/null

--- dozzle.sh
+++ dozzle.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+echo  _____               _      
+echo |  __ \             | |     
+echo | |  | | ___ _______| | ___ 
+echo | |  | |/ _ \_  /_  / |/ _ \
+echo | |__| | (_) / / / /| |  __/
+echo |_____/ \___/___/___|_|\___|
+                             
 mkdir -p /data/dozzle
 
 echo "made /data/dozzle"
 
 cd /data/dozzle
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/dozzle.yml
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/dozzle.yml > /dev/null
 
 echo "downloaded docker-compose.yml"
 
 docker compose up -d
 
 echo "finished install dozzle"
-
-echo "dozzle is ip:9999"
-
-echo "exiting..."
 
 exit
