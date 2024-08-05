4add104ef79b52cf098c7ceaf1b911f231e526da
Ryuyijun <ryuyijun@naver.com>
2024-08-05 23:05:23 +0900

    Update dockge.sh
    add ascii, wget > /dev/null

--- dockge.sh
+++ dockge.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+echo  _____             _              
+echo |  __ \           | |             
+echo | |  | | ___   ___| | ____ _  ___ 
+echo | |  | |/ _ \ / __| |/ / _` |/ _ \
+echo | |__| | (_) | (__|   < (_| |  __/
+echo |_____/ \___/ \___|_|\_\__, |\___|
+echo                         __/ |     
+echo                        |___/      
+
 mkdir -p /opt/stacks /opt/dockge
 
 echo "made /opt/stacks /opt/dockge"
 
 cd /opt/dockge
 
-wget https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml
+wget https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml > /dev/null
 
 echo "downloaded compose.yml"
 
 docker compose up -d
 
 echo "finished install dockge"
-
-echo "dockge is ip:5001"
-
-echo "exiting..."
 
 exit
