38076404eae1dcb399bb247aebd1f1f58de03723
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:15:13 +0900

    Update dozzle.sh

--- dozzle.sh
+++ dozzle.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo  _____               _      
-echo |  __ \             | |     
-echo | |  | | ___ _______| | ___ 
-echo | |  | |/ _ \_  /_  / |/ _ \
-echo | |__| | (_) / / / /| |  __/
-echo |_____/ \___/___/___|_|\___|
-                             
+echo " _____               _      "
+echo "|  __ \             | |     "
+echo "| |  | | ___ _______| | ___ "
+echo "| |  | |/ _ \_  /_  / |/ _ \\"
+echo "| |__| | (_) / / / /| |  __/"
+echo "|_____/ \___/___/___|_|\___|"
+
 mkdir -p /data/dozzle
 
 echo "made /data/dozzle"
 
 cd /data/dozzle
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/dozzle.yml > /dev/null
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/dozzle.yml > /dev/null 2>&1
 
 echo "downloaded docker-compose.yml"
 
