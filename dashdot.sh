449e98aea3daa7dd1d28e151129e12901f55c4d8
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:13:37 +0900

    Update dashdot.sh

--- dashdot.sh
+++ dashdot.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo ' _____            _         _       _ '  
-echo '|  __ \          | |       | |     | | ' 
-echo '| |  | | __ _ ___| |__   __| | ___ | |_ '
-echo '| |  | |/ _` / __| '_ \ / _` |/ _ \| __|'
-echo '| |__| | (_| \__ \ | | | (_| | (_) | |_ '
-echo '|_____/ \__,_|___/_| |_|\__,_|\___/ \__|'
+echo " _____            _         _       _ "
+echo "|  __ \          | |       | |     | | "
+echo "| |  | | __ _ ___| |__   __| | ___ | |_ "
+echo "| |  | |/ _\` / __| '_ \ / _\` |/ _ \| __|"
+echo "| |__| | (_| \__ \ | | | (_| | (_) | |_ "
+echo "|_____/ \__,_|___/_| |_|\__,_|\___/ \__|"
 
-docker container run -d -it --restart always  -p 3002:3001  -v /etc/os-release:/etc/os-release:ro --privileged   -e 'DASHDOT_OVERRIDE_OS "{debian 12}"'   --name dashdot   mauricenino/dashdot
+docker container run -d -it --restart always -p 3002:3001 -v /etc/os-release:/etc/os-release:ro --privileged -e "DASHDOT_OVERRIDE_OS {debian 12}" --name dashdot mauricenino/dashdot
 
 echo "finished install dashdot"
-
-echo "dashdot is ip:3002"
-
-echo "exiting..."
 
 exit
