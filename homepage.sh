eea0502da18a896d956863a88d44a750fabd61d7
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:19:55 +0900

    Update homepage.sh

--- homepage.sh
+++ homepage.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-echo  _    _                                             
-echo | |  | |                                            
-echo | |__| | ___  _ __ ___   ___ _ __   __ _  __ _  ___ 
-echo |  __  |/ _ \| '_ ` _ \ / _ \ '_ \ / _` |/ _` |/ _ \
-echo | |  | | (_) | | | | | |  __/ |_) | (_| | (_| |  __/
-echo |_|  |_|\___/|_| |_| |_|\___| .__/ \__,_|\__, |\___|
-echo                             | |           __/ |     
-echo                             |_|          |___/      
+echo "_    _                                              "
+echo "| |  | |                                            "
+echo "| |__| | ___  _ __ ___   ___ _ __   __ _  __ _  ___ "
+echo "|  __  |/ _ \| '_ ` _ \ / _ \ '_ \ / _` |/ _` |/ _ \\"
+echo "| |  | | (_) | | | | | |  __/ |_) | (_| | (_| |  __/"
+echo "|_|  |_|\___/|_| |_| |_|\___| .__/ \__,_|\__,_|\___|"
+echo "                            | |           __/ |     "
+echo "                            |_|          |___/      "
 
 mkdir -p /data/homepage/config
 
-echo 'made /data/homepage/config'
+echo "made /data/homepage/config"
 
 cd /data/homepage/
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homepage.yml > /dev/null
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/homepage.yml > /dev/null 2>&1
 
-echo 'downloaded docker-compose.yml'
+echo "downloaded docker-compose.yml"
 
 docker compose up -d
 
-echo 'finished'
+echo "finished"
 
 exit
-
