4498c04d75385b873e0c7007fb22606a58a2ff29
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:20:23 +0900

    Update npmplus.sh

--- npmplus.sh
+++ npmplus.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-echo  _   _ _____  __  __        _____  _           
-echo | \ | |  __ \|  \/  |      |  __ \| |          
-echo |  \| | |__) | \  / |______| |__) | |_   _ ___ 
-echo | . ` |  ___/| |\/| |______|  ___/| | | | / __|
-echo | |\  | |    | |  | |      | |    | | |_| \__ \
-echo |_| \_|_|    |_|  |_|      |_|    |_|\__,_|___/
-                                                                                                
+echo "_   _ _____  __  __        _____  _           "
+echo "| \ | |  __ \|  \/  |      |  __ \| |          "
+echo "|  \| | |__) | \  / |______| |__) | |_   _ ___  "
+echo "| . ` |  ___/| |\/| |______|  ___/| | | | / __| "
+echo "| |\  | |    | |  | |      | |    | | |_| \__ \ "
+echo "|_| \_|_|    |_|  |_|      |_|    |_|\__,_|___/ "
+
 mkdir -p /data/npmplus
 
 echo "made /data/npmplus"
 
 cd /data/npmplus
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/npmplus.yml > /dev/null
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/npmplus.yml > /dev/null 2>&1
 
 echo "downloaded docker-compose.yml"
 
 docker compose up -d
 
-echo "finishing install nginxproxymanager-plus"
+echo "finished installing nginxproxymanager-plus"
 
 exit
-
-
