09b34c4723114d4c2815ff6a9e84ec61d1048332
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:18:04 +0900

    Update filebrowser.sh

--- filebrowser.sh
+++ filebrowser.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo ______ _ _      _                                     
-echo |  ____(_) |    | |                                    
-echo | |__   _| | ___| |__  _ __ _____      _____  ___ _ __ 
-echo |  __| | | |/ _ \ '_ \| '__/ _ \ \ /\ / / __|/ _ \ '__|
-echo | |    | | |  __/ |_) | | | (_) \ V  V /\__ \  __/ |   
-echo |_|    |_|_|\___|_.__/|_|  \___/ \_/\_/ |___/\___|_|   
-                                                                                                            
+echo " ______ _ _      _                                     "
+echo "|  ____(_) |    | |                                    "
+echo "| |__   _| | ___| |__  _ __ _____      _____  ___ _ __ "
+echo "|  __| | | |/ _ \ '_ \| '__/ _ \ \ /\ / / __|/ _ \ '__|"
+echo "| |    | | |  __/ |_) | | | (_) \ V  V /\__ \  __/ |   "
+echo "|_|    |_|_|\___|_.__/|_|  \___/ \_/\_/ |___/\___|_|   "
+
 mkdir -p /data/filebrowser/config
 
 echo "made necessary directory"
@@ -17,13 +17,13 @@ echo "made empty database.db"
 
 cd /data/filebrowser/config
 
-wget https://raw.githubusercontent.com/r1614/setup/main/filebrowser.json > /dev/null
+wget https://raw.githubusercontent.com/r1614/setup/main/filebrowser.json > /dev/null 2>&1
 
 echo "downloaded filebrowser.json"
 
 cd /data/filebrowser
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/filebrowser.yml > /dev/null
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/filebrowser.yml > /dev/null 2>&1
 
 echo "downloaded docker-compose.yml"
 
