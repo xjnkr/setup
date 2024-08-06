d1d48159bf39b5cef724fac6f34d308eef34e599
Ryuyijun <ryuyijun@naver.com>
2024-08-06 10:17:22 +0900

    Update excalidraw.sh

--- excalidraw.sh
+++ excalidraw.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo  ______               _ _     _                    
-echo |  ____|             | (_)   | |                   
-echo | |__  __  _____ __ _| |_  __| |_ __ __ ___      __
-echo |  __| \ \/ / __/ _` | | |/ _` | '__/ _` \ \ /\ / /
-echo | |____ >  < (_| (_| | | | (_| | | | (_| |\ V  V / 
-echo |______/_/\_\___\__,_|_|_|\__,_|_|  \__,_| \_/\_/  
-                                                                                                     
+echo " ______               _ _     _                    "
+echo "|  ____|             | (_)   | |                   "
+echo "| |__  __  _____ __ _| |_  __| |_ __ __ ___      __"
+echo "|  __| \ \/ / __/ _\` | | |/ _\` | '__/ _\` \\ \\ /\\ / /"
+echo "| |____ >  < (_| (_| | | | (_| | | | (_| |\\ V  V / "
+echo "|______/_/\\_\\___\\__,_|_|_|\\__,_|_|  \\__,_| \\_/\\_/  "
+
 mkdir -p /data/excalidraw
 
 cd /data/excalidraw
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/excalidraw.yml > /dev/null
+wget -O docker-compose.yml https://raw.githubusercontent.com/r1614/setup/main/excalidraw.yml > /dev/null 2>&1
 
 echo "downloaded docker-compose.yml"
 
